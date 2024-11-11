32e1298151e43451db51fb2ec48f506c606efaa3
Yi Ywe Lin <73067851+yiywelin@users.noreply.github.com>
2024-11-11 21:08:24 +0800

    Cleaned code

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -76,7 +76,6 @@ const navLinks = [
   { name: 'Swipe Movies', path: '/select-genre' },
   { name: 'Watch with Friends', path: '/friends' },
   { name: 'About Us', path: '/meet-the-team' },
-  { name: 'Movie Roulette', path: '/movie-roulette' },
 ];
 
 onMounted(() => {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'; // Import the router
+import router from './router';
 import './assets/tailwind.css'
 
 createApp(App).use(router).mount('#app');
